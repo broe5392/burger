292ca0281ec4fc98e069317a8913642221138fc7
Brandon B <broe5392@yahoo.com>
2018-08-23 14:00:36 -0400

    basic berger app

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES("Drunken Monk", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES("Engine Oil", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES("Along Came Danny", FALSE);
